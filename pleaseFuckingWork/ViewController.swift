d838ec14aa0f9c23bcff8b5571b1a51fe3f1581f
Joseph James (s5116508) <s5116508@mac38576.student.bournemouth.ac.uk>
2018-11-26 17:40:25 +0000

    Initial Commit

---
+++ pleaseFuckingWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pleaseFuckingWork
+//
+//  Created by Joseph James (s5116508) on 26/11/2018.
+//  Copyright © 2018 Joseph James (s5116508). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
