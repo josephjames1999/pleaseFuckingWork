125ee2fab2ea93d78ad38bc62d672c5633424a9d
Joseph James (s5116508) <s5116508@mac42936.student.bournemouth.ac.uk>
2018-11-29 12:37:11 +0000

    Commented in the location stuff

--- pleaseFuckingWork/MainViewController.swift
+++ pleaseFuckingWork/MainViewController.swift
@@ -1,10 +1,46 @@
 import UIKit
+import MapKit
 
 class MainViewController: UIViewController{
     
-    //override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-      //  AppManager.shared.logout()
-    //}
+    @IBOutlet var mapView: MKMapView!
     
+    override func viewDidLoad() {
+      super.viewDidLoad()
+//
+//        mapView.userTrackingMode = .follow
+//
+        //locationManager.requestAlwaysAuthorization()
+        //locationManager.delegate = self
+        //locationManager.startUpdatingLocation()
     
+        //override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+          //AppManager.shared.logout()
+//        }
+    
+   // extension ViewController: MKMapViewDelegate{
+
+//        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//
+//            //guard let annotation = annotation as? Treasure else { return nil }
+//
+//            let identifier = "marker"
+//            var view: MKMarkerAnnotationView
+//
+//            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+//                as? MKMarkerAnnotationView {
+//                dequeuedView.annotation = annotation
+//                view = dequeuedView
+//            } else {
+//                view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+//
+//            }
+//
+//            return view
+//
+//
+//        }
+    
+    }
+
 }
