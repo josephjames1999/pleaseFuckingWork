414f4974b94f600dded43f9eb3b0bb964ec81964
Joseph James (s5116508) <s5116508@mac42936.student.bournemouth.ac.uk>
2018-11-29 11:48:53 +0000

    added dummy data to login

--- pleaseFuckingWork/LoginViewController.swift
+++ pleaseFuckingWork/LoginViewController.swift
@@ -6,6 +6,12 @@ class LoginViewController: UIViewController{
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordViewController: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.text = "test@test.com"
+        passwordViewController.text = "password"
+    }
+    
     @IBAction func login(_ sender: Any) {
         
         guard let email = emailTextField.text, let password = passwordViewController.text else { return }
