6bc8056bfe62d0e7c821710e1b7a50ad0b923ab3
Charleigh-Ann Jenkins (s4908563) <s4908563@mac42945.student.bournemouth.ac.uk>
2018-12-06 12:13:44 +0000

    pushing again for no reason

---
+++ pleaseFuckingWork/Annotation.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+import MapKit
+
+class CustomAnnotation: NSObject, MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    
+    init(address: Address) {
+        self.coordinate = CLLocationCoordinate2D(latitude: address.latitude!, longitude: address.longitude!)
+        self.title = address.title
+    }
+    
+    
+}
+
+class Address: NSObject {
+    var latitude:CLLocationDegrees?
+    var longitude:CLLocationDegrees?
+    var title = ""
+}
